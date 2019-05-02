640511393d849dad9504a6027d96ecffad492456
Коковин Алексей Николаевич <ankokovin@edu.hse.ru>
2019-05-02 21:12:58 +0500

    Create Compare.cs

---
+++ BlocklyForHouseTransformer/XmlToPython/Blocks/Logic/Compare.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+
+namespace BlocklyForHouse.Transform.XmlToPython.Blocks.Logic
+{
+    
+    class Compare : Block
+    {
+        public override string Interpret(ref XmlToPythonContext context)
+        {
+            base.Interpret(ref context);
+            var nodes = context.currentNode.ChildNodes;
+            string OP = string.Empty;
+            string A = string.Empty;
+            string B = string.Empty;
+            foreach (XmlNode n in nodes)
+            {
+                context.currentNode = n;
+                switch (n.Attributes["name"].Value)
+                {
+                    case "OP":
+                        OP = new Field().Interpret(ref context);
+                        break;
+
+                    //TODO: add attribute for comparables
+                    case "A":
+                        A = new Math.Number().Interpret(ref context);
+                        break;
+                    case "B":
+                        B = new Math.Number().Interpret(ref context);
+                        break;
+                    default:
+                        throw new System.Exception();
+                }
+            }
+            //TODO: error handling
+            var result = "(" + A + Operations[OP] + B + ")";
+            context.ParentNode();
+            return result;
+        }
+
+        private readonly Dictionary<string, string> Operations = new Dictionary<string, string>
+        {
+            ["EQ"] = "==",
+            ["NEQ"] = "!=",
+            ["LT"] = "<",
+            ["LTE"] = "<=",
+            ["GT"] = ">",
+            ["GTE"] = ">=",
+        };
+    }
+}
