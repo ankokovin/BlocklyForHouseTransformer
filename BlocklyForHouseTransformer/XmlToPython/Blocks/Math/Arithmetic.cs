a5b0016edb7650c70feca5fd34408b260f9d8017
Коковин Алексей Николаевич <ankokovin@edu.hse.ru>
2019-05-02 20:57:49 +0500

    Added math_number and math_arithmetic

---
+++ BlocklyForHouseTransformer/XmlToPython/Blocks/Math/Arithmetic.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Xml;
+
+namespace BlocklyForHouse.Transform.XmlToPython.Blocks.Math
+{
+    /// <summary>
+    /// <para>&lt;block type=&quot;math_arithmetic&quot;&gt;</para>
+    /// </summary>
+    public class Arithmetic : Block
+    {
+        /// <summary>
+        /// Interpret context as <see cref="Arithmetic"/>
+        /// </summary>
+        /// <param name="context">Context to interpret</param>
+        /// <returns>Python code</returns>
+        public override string Interpret(ref XmlToPythonContext context)
+        {
+            base.Interpret(ref context);
+            var nodes = context.currentNode.ChildNodes;
+            string OP = string.Empty;
+            string A = string.Empty;
+            string B = string.Empty;
+            foreach (XmlNode n in nodes)
+            {
+                context.currentNode = n;
+                switch (n.Attributes["name"].Value)
+                {
+                    case "OP":
+                        OP = new Field().Interpret(ref context);
+                        break;
+                    case "A":
+                        A = new Number().Interpret(ref context);
+                        break;
+                    case "B":
+                        B = new Number().Interpret(ref context);
+                        break;
+                    default:
+                        throw new System.Exception();
+                }
+            }
+            //TODO: error handling
+            var result = "("+A + Operations[OP] + B+")";
+            context.ParentNode();
+            return result;
+        }
+
+        private readonly Dictionary<string, string> Operations = new Dictionary<string, string>
+        {
+            ["ADD"] = "+",
+            ["MINUS"] = "-",
+            ["MULTIPLY"] = "*",
+            ["DIVIDE"] = "/",
+            ["POWER"] = "**",
+        };
+    }
+}
---
+++ BlocklyForHouseTransformer/XmlToPython/Blocks/Math/Number.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlocklyForHouse.Transform.XmlToPython.Blocks.Math
+{
+    /// <summary>
+    /// <para>&lt;block type=&quot;math_number&quot;&gt;</para>
+    /// </summary>
+    public class Number : Block
+    {
+        /// <summary>
+        /// Interpret context as <see cref="Number"/>
+        /// </summary>
+        /// <param name="context">Context to interpret</param>
+        /// <returns>Python code</returns>
+        public override string Interpret(ref XmlToPythonContext context)
+        {
+            base.Interpret(ref context);
+            context.NextNode();
+            var result = new Field().Interpret(ref context);
+            context.ParentNode();
+            return result;
+        }
+    }
+}
