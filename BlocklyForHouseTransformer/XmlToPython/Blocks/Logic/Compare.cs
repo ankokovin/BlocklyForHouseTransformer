44dfab436f4902a2112c9fdd906b5c09cd842a95
Коковин Алексей Николаевич <ankokovin@edu.hse.ru>
2019-05-03 11:51:46 +0500

    compare block to use attributes

--- BlocklyForHouseTransformer/XmlToPython/Blocks/Logic/Compare.cs
+++ BlocklyForHouseTransformer/XmlToPython/Blocks/Logic/Compare.cs
@@ -5,7 +5,7 @@ using System.Xml;
 
 namespace BlocklyForHouse.Transform.XmlToPython.Blocks.Logic
 {
-    [Custom_attributes.BoolneanAttribute("logic_compare")]
+    [Custom_attributes.Boolnean("logic_compare")]
     class Compare : Block
     {
         public override string Interpret(ref XmlToPythonContext context)
@@ -26,10 +26,22 @@ namespace BlocklyForHouse.Transform.XmlToPython.Blocks.Logic
 
                     //TODO: add attribute for comparables
                     case "A":
-                        A = new Math.Number().Interpret(ref context);
+                        context.NextNode();
+                        if (OP == "EQ" || OP == "NEQ")
+                            A = EqBlocks[context.currentNode.Attributes["type"].Value]
+                                .Interpret(ref context);
+                        else
+                            A = ComBlocks[context.currentNode.Attributes["type"].Value]
+                                .Interpret(ref context);
                         break;
                     case "B":
-                        B = new Math.Number().Interpret(ref context);
+                        context.NextNode();
+                        if (OP == "EQ" || OP == "NEQ")
+                            B = EqBlocks[context.currentNode.Attributes["type"].Value]
+                                .Interpret(ref context);
+                        else
+                            B = ComBlocks[context.currentNode.Attributes["type"].Value]
+                                .Interpret(ref context);
                         break;
                     default:
                         throw new System.Exception();
@@ -50,5 +62,10 @@ namespace BlocklyForHouse.Transform.XmlToPython.Blocks.Logic
             ["GT"] = ">",
             ["GTE"] = ">=",
         };
+
+        private readonly Dictionary<string, Block> ComBlocks =
+            Custom_attributes.FinderAttribute.GetBlocks<Custom_attributes.ComparableAttribute>();
+        private readonly Dictionary<string, Block> EqBlocks =
+            Custom_attributes.FinderAttribute.GetBlocks<Custom_attributes.EquitableAttribute>();
     }
 }
